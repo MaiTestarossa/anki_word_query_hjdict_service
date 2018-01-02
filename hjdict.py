8275de419126b072fdf166bfc572053e61eedb5b
Mai <dev.testarossa@gmail.com>
2018-01-02 20:56:43 +0800

    Add files via upload

--- hjdict.py
+++ hjdict.py
@@ -101,7 +101,7 @@ class hjdict(WebService):
         urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
 
         audio_url = self.cache_result('mp3')
-        showInfo(audio_url)
+        # showInfo(audio_url)
         filename = u'_hj_{}.mp3'.format(self.word)
         try:
             urllib.urlretrieve(audio_url, filename)
